9359c07aaa728a3761e2fc9d30a684292c1bfdef
choidoorim <doorim321@naver.com>
2024-05-11 20:04:13 +0900

    feat: mysql docker compose 파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `environment/.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    HealthModule,
+  ],
 })
 export class AppModule {}

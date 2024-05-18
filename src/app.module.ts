4dff82b2caedd68c0b45022b8d9cf27cbb2de85f
choidoorim <doorim321@naver.com>
2024-05-18 19:07:43 +0900

    feat: application execution script

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { HealthModule } from './health/health.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `environment/.env.${process.env.NODE_ENV}`,
+      envFilePath: `environments/.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
     PrismaModule,
